db5bc1b9de1d89fd616b6612cb25e0b2170c5f2d
Peter1907 <peter.nady.1408@gmail.com>
2022-11-12 02:06:50 +0200

    Add print statements to 'my_list' file

--- my_list.rb
+++ my_list.rb
@@ -1,12 +1,16 @@
+require './my_enumerable'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
-list.filter { |e| e.even? }
+p (list.all? { |e| e < 5 })
+p (list.all? { |e| e > 5 })
+p (list.any? { |e| e == 2 })
+p (list.any? { |e| e == 5 })
+p (list.filter(&:even?))
